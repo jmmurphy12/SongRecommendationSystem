64e09efc97d6d7487f821c14e03d79a954f62122
Josh Murphy <jmmurphy@maple.rlogin>
2023-10-16 16:35:34 -0400

    Added another basic data manipulation function

--- data_retrival.py
+++ data_retrival.py
@@ -6,18 +6,18 @@ def GetUserData(id):
     result = []  # A list to store the tuples
 
     with open('hetrec2011-lastfm-2k/user_artists.dat', 'r') as file:
-        first_line = True  # Track if it's the first line
+        first_line = True 
         for line in file:
             if first_line:
                 first_line = False
-                continue  # Skip the first line
+                continue 
 
             numbers = line.strip().split()
 
             if len(numbers) != 3:
                 continue
 
-            num1, num2, num3 = map(int, numbers)  # Convert to integers
+            num1, num2, num3 = map(int, numbers) 
 
             if id == num1:
                 result.append((num2, num3))
@@ -47,6 +47,30 @@ def GetArtistName(id):
 
     return "Name not in database"
 
+"""This Function gets the artists id based off of the artists name"""
+def GetArtistId(name):
+    with open('hetrec2011-lastfm-2k/artists.dat', 'r') as file:
+        first_line = True
+        for line in file:
+            if first_line:
+                first_line = False
+                continue
+
+            data = line.strip().split()
+            temp_name = ""
+            for word in data[1:]:
+                if "http" not in word: 
+                    if temp_name == "":
+                        temp_name = temp_name + word
+                    else:
+                        temp_name = temp_name + " " + word
+
+            if name == temp_name:
+                return data[0]
+
+    return "Name not in database"
+
 
 print(GetUserData(3))
 print(GetArtistName(1))
+print(GetArtistId("MALICE MIZER"))
