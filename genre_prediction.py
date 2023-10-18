4d46a2f4978f5f637915febcf25aac8f7c64aef1
Josh Murphy <jmmurphy@hickory.rlogin>
2023-10-18 19:23:26 -0400

    Added spotify creditials

---
+++ genre_prediction.py
@@ -0,0 +1,26 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Step 1: Set up your Spotify Developer credentials
+client_id = 'd3f16a00c7c344d88953f96a3bc04e66'
+client_secret = 'b87120c44b14432bb90e0a6dd13234af'
+
+# Step 2: Authenticate your application
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                     client_secret=client_secret, redirect_uri='http://localhost:3000'))
+
+# Step 3: Retrieve a list of genres
+genres = sp.recommendation_genre_seeds()
+
+# Step 4: Fetch the top 200 songs in each genre
+top_tracks = {}
+
+for genre in genres['genres']:
+    top_tracks[genre] = sp.recommendations(
+        seed_genres=[genre], limit=200)['tracks']
+
+
+
+
+# Now, top_tracks is a dictionary where each key is a genre, and the value is a list of the top 200 tracks in that genre.
+# You can process this data further as needed.
---
+++ main.py
@@ -0,0 +1,6 @@
+import data_retrival
+
+# Specify the user we would like to look at 
+
+# Pass this value in to the song recomender function
+
